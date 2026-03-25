b3cb29ec127af4569409a30a4fec8f958dcab1bb
Lukas Nitzsche <lukasnitzsche@yahoo.com>
2026-03-25 13:38:30 +0100

    test functions rather than main func

--- fibonacci.rs
+++ fibonacci.rs
@@ -7,7 +7,8 @@ fn fibonacci(n: u64) -> u64 {
   //}
 }
 
-fn main() {
+#[test]
+fn test_fib() {
   assert_eq!(
     (1..=20).map(|i| fibonacci(i)).collect::<Vec<u64>>(),
     &[1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765],
--- unbounded_knapsack.rs
+++ unbounded_knapsack.rs
@@ -9,13 +9,20 @@ fn count_change(money: i32, coins: &[i32]) -> i32 {
   }
 }
 
-fn main() {
+#[test]
+fn test_count_change_1() {
   assert_eq!(count_change(1, &[1,2]), 1);
   assert_eq!(count_change(1, &[2,1]), 1);
+}
 
+#[test]
+fn test_count_change_2() {
   assert_eq!(count_change(4, &[2,1]), 3);
   assert_eq!(count_change(4, &[1,2]), 3);
+}
 
+#[test]
+fn test_count_change_3() {
   assert_eq!(count_change(4, &[3,1,2]), 4);
   assert_eq!(count_change(4, &[2,3,1]), 4);
   assert_eq!(count_change(4, &[2,1,3]), 4);
